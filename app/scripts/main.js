602906911a20b2d85aa20a978720702444ad61ad
Steve Crockett <crockett95@gmail.com>
2014-11-24 16:44:54 -0600

    Working Kanban

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,52 @@
-jQuery(document).ready(function () {
+require.config({
+  paths: {
+    'jquery': '../bower_components/jquery/dist/jquery.min',
+    'jquery-ui': '../bower_components/jquery-ui/ui',
+    'bootstrap': '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap'
+  },
+  shim: {
+    'bootstrap': {
+      deps: ['jquery']
+    }
+  }
+});
+require([
+  'jquery',
+  'jquery-ui/draggable',
+  'jquery-ui/droppable',
+  'jquery-ui/sortable',
+  'bootstrap'
+], function ($) {
   'use strict';
 
   var activeNode;
 
-  $('.panel').on('dragstart', function (e) {
-    console.log(e);
-    $(this).addClass('dragging');
-    activeNode = this;
-  }).on('dragend', function () {
-    $(this).removeClass('dragging');
+  $('.panel').draggable({
+    appendTo: 'body',
+    connectToSortable: '.column-inner',
+    start: function (event, ui) {
+      var $this = $(this),
+          $parent = $this.parent();
+
+      if ($parent.children().length === 1) {
+        $('<p></p>').addClass('empty-column').css('visibility', 'hidden').text('Empty').appendTo($parent);
+      }
+    }
   });
 
-  $('.kanban-board').find('.kanban-column').on('dragover', function (e) {
-    if (e.originalEvent.preventDefault) {
-      e.preventDefault();
+  $('.kanban-board .kanban-column').droppable({
+    accept: ':not(.ui-sortable-helper)',
+    drop: function (event, ui) {
+      $(this).find('.empty-column').remove();
+      ui.draggable.appendTo($(this).find('.column-inner'));
     }
+  });
 
-    e.originalEvent.dataTransfer.dropEffect = 'move';
-
-    $(this).addClass('over');
-  }).on('dragleave', function () {
-    $(this).removeClass('over');
-  }).on('drop', function (e) {
-    if (e.originalEvent.stopPropagation) {
-      e.originalEvent.stopPropagation(); // stops the browser from redirecting.
+  $('.column-inner').sortable({
+    items: '.panel',
+    sort: function (event, ui) {
+      ui.item.css('height', '').css('width', '');
     }
-
-    var $this = $(this),
-        element = activeNode,
-        columnInner = $this.find('.column-inner');
-
-    $(element).appendTo(columnInner);
-
-    $this.removeClass('over');
   });
 
 });
